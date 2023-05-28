cbefcf4ec93aa06fbfc5b7691df535d4efc4de65
Kuldeep Singh <ksingh51@uic.edu>
2023-05-28 18:19:27 -0500

    Completed the About, Experience and Nav Section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook } from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
+import React, { useState } from 'react';
+import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BiBook } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import { BiMessageSquareDetail } from 'react-icons/bi';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
+  const handleNavClick = (nav) => {
+    setActiveNav(nav);
+  };
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BiMessageSquareDetail/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ''} onClick={() => handleNavClick('#')}><AiOutlineHome/></a>
+      <a href="#about" className={activeNav === '#about' ? 'active' : ''} onClick={() => handleNavClick('#about')}><AiOutlineUser/></a>
+      <a href="#experience" className={activeNav === '#experience' ? 'active' : ''} onClick={() => handleNavClick('#experience')}><BiBook/></a>
+      <a href="#services" className={activeNav === '#services' ? 'active' : ''} onClick={() => handleNavClick('#services')}><RiServiceLine/></a>
+      <a href="#contact" className={activeNav === '#contact' ? 'active' : ''} onClick={() => handleNavClick('#contact')}><BiMessageSquareDetail/></a>
     </nav>
-  )
-}
+  );
+};
 
 export default Nav
